2fb758bacb95867524d649d887aa41e5be0b1033
blupage <73701631+blupage@users.noreply.github.com>
2022-10-28 22:57:42 +0530

    Update rotate_matrix.py

--- Intermediate DSA/2D Matrices/rotate_matrix.py
+++ Intermediate DSA/2D Matrices/rotate_matrix.py
@@ -3,3 +3,14 @@
 # You need to do this in place.
 # Note: If you end up using an additional array, you will only receive partial score.
 
+class Solution:
+    def solve(self, matrix):
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+        return matrix
