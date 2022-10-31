3372866b237305f4a9aa8e9427deed986af6996f
yash-wins <yashwinsaini@gmail.com>
2022-10-31 16:14:47 +0530

    update

--- Intermediate DSA/Bit Manipulations/subarrays_with_bitwise_or.py
+++ Intermediate DSA/Bit Manipulations/subarrays_with_bitwise_or.py
@@ -1,1 +1,13 @@
 # Given an array B of length A with elements 1 or 0. Find the number of subarrays with bitwise OR 1.
+
+class Solution:
+    # @param A : integer
+    # @param B : list of integers
+     # @return an long
+    def solve(self, A, B):
+        count = 0
+        for i in range(A):
+            for j in range(i,A):
+                if B[i] | B[j]:
+                    count += 1
+        return count
